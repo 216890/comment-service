31dea99b1c9641aef877c37991e26ba76c96b711
216890 <%NY532s2aC$Y3D>
2021-11-20 20:11:28 +0100

    add application tag

--- src/main/kotlin/pl/lodz/p/stanczyk/commentservice/config/ApplicationConfig.kt
+++ src/main/kotlin/pl/lodz/p/stanczyk/commentservice/config/ApplicationConfig.kt
@@ -1,5 +1,8 @@
 package pl.lodz.p.stanczyk.commentservice.config
 
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import pl.lodz.p.stanczyk.commentservice.adapter.MongoCommentRepositoryAdapter
@@ -23,4 +26,10 @@ class ApplicationConfig {
 
     @Bean
     fun instantNowSupplier(): InstantNowSupplier = InstantNowSupplierImpl()
+
+    @Bean
+    fun metricsCommonTags(@Value("spring.application.name") applicationName: String): MeterRegistryCustomizer<MeterRegistry> =
+        MeterRegistryCustomizer { registry: MeterRegistry ->
+            registry.config().commonTags("application", applicationName)
+        }
 }
